e4af2a91c266e11763d1ba73fb230b1c81523f26
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2023-10-24 22:57:33 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,39 +1,20 @@
+let settingsButton;
+let snakeGameButton;
+let typingGameButton;
+
 function setup() {
-  createCanvas(500,500);
-  buttons();
-}
-function mainScreen() {
-  background(400,100,100);
-  strokeWeight(5); 
-  rect(-10,-10,600,85)
-  fill(0, 102, 153);
-  textSize(40)
-  text('Curable',177, 50);
-  fill(0,200,500);
-  textFont('Palatino')
-}
-var mode = "mainScreen";
-function draw() {
- if (mode == "mainScreen") {
-    mainScreen();
-  } else if (mode == "profileScreen") {
-   profileScreen();
-  } else if (mode == "settingsScreen") {
-    settingsScreen();
-  } else if (mode == "FIXME") {
-    screen4();
-  } else if (mode == "FIXME") {
-    screen4();
-  } else if (mode == "FIXME") {
-    screen4();
-  }
-  
+  noCanvas();
+
+  settingsButton = createButton('Settings');
+  settingsButton.mousePressed(() => window.location.href='settings/index.html');
+
+  snakeGameButton = createButton('Snake Game!');
+  snakeGameButton.mousePressed(() => window.location.href='snakeGame/snakeGame.html');
+
+  typingGameButton = createButton('Typing Game!');
+  typingGameButton.mousePressed(() => window.location.href='typingGame/typingGame.html');
 }
 
-function buttons() {
-  buttonTypingGame = createButton('Typing Game');
-  buttonTypingGame.size(400,75);
-  buttonTypingGame.position(50,100);
-  buttonTypingGame.style('font-size','24px');
-  buttonTypingGame.style('font-text','Garamont')
+function draw() {
+  // put drawing code here
 }
