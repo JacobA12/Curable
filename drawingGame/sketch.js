62ceac305787f5c1d130d6ec1d5b59d7d84bd079
JacobA12 <jacob.adams121202@gmail.com>
2023-11-15 18:43:19 -0700

    added more logic and score

--- drawingGame/sketch.js
+++ drawingGame/sketch.js
@@ -1,75 +1,109 @@
-let scoreElem;
 let circles = [];
 let accurCircles = [];
 let startTime = 0;
 let timeToAdd = 4000; // 4 seconds
 let drawnLine;
+let currentOrder = 0;
+let accuracy = 100;
 
-let score = 0;
 
 function setup() {
   createCanvas(700, 450);
   background(0);
   noSmooth();
   strokeWeight(7);
-  scoreElem = createDiv("Score = 0");
-  scoreElem.position(20, 20);
-  scoreElem.id = "score";
-  scoreElem.style("color", "white");
+
 
   // Create rollover circles
   //circle 1 to 2
-  circles.push(createCircle(200, 100, 20, "1"));
-  accurCircles.push(createAccurCircle(190, 120, 20, "1.0"));
-  accurCircles.push(createAccurCircle(180, 140, 20, "1.1"));
-  accurCircles.push(createAccurCircle(170, 160, 20, "1.2"));
-  accurCircles.push(createAccurCircle(160, 180, 20, "1.3"));
-  
+  circles.push(createCircle(200, 100, 20, "1", 0));
+  accurCircles.push(createAccurCircle(190, 120, 20, "1.0", 1));
+  accurCircles.push(createAccurCircle(180, 140, 20, "1.1", 2));
+  accurCircles.push(createAccurCircle(170, 160, 20, "1.2", 3));
+  accurCircles.push(createAccurCircle(160, 180, 20, "1.3", 4));
+
   //circle 2 to 3
-  circles.push(createCircle(150, 200, 20, "2"));
+  circles.push(createCircle(150, 200, 20, "2", 5));
   for (let index = 0; index < 4; index++) {
-    accurCircles.push(createAccurCircle(140 - 10 * index, 220 + 20 * index, 20, "2."+ index.toString()))
+    accurCircles.push(
+      createAccurCircle(
+        140 - 10 * index,
+        220 + 20 * index,
+        20,
+        "2." + index.toString(),
+        6 + index
+      )
+    );
   }
   //circle 3 to 4
-  circles.push(createCircle(100, 300, 20, "3"));
+  circles.push(createCircle(100, 300, 20, "3", 10));
   for (let index = 0; index < 4; index++) {
-    accurCircles.push(createAccurCircle(120+20*index,300,20,"3."+ index.toString()))
-  }
-  //circles 4 to 5
-  circles.push(createCircle(200, 300, 20, "4"));
-  for (let index = 0; index < 4; index++) {
-    accurCircles.push(createAccurCircle(220+20*index,300,20,"4."+ index.toString()))
-  }
-  //circles 5 to 6
-  circles.push(createCircle(300, 300, 20, "5"));
-  for (let index = 0; index < 4; index++) {
-    accurCircles.push(createAccurCircle(290 - 10 * index, 280 - 20 * index, 20, "5." + index.toString()))
-  }
-  //circles 6 to 1
-  circles.push(createCircle(250, 200, 20, "6"));
-  for (let index = 0; index < 4; index++) {
-    accurCircles.push(createAccurCircle(240 - 10 * index, 180 - 20 * index, 20, "6."+ index.toString()))
+    accurCircles.push(
+      createAccurCircle(
+        120 + 20 * index,
+        300,
+        20,
+        "3." + index.toString(),
+        11 + index
+      )
+    );
+
+    //circles 4 to 5
+    circles.push(createCircle(200, 300, 20, "4", 15));
+    for (let index = 0; index < 4; index++) {
+      accurCircles.push(
+        createAccurCircle(220 + 20 * index, 300, 20, "4." + index.toString()),
+        16 + index
+      );
+    }
+    //circles 5 to 6
+    circles.push(createCircle(300, 300, 20, "5"), 20);
+    for (let index = 0; index < 4; index++) {
+      accurCircles.push(
+        createAccurCircle(
+          290 - 10 * index,
+          280 - 20 * index,
+          20,
+          "5." + index.toString()
+        ),
+        21 + index
+      );
+    }
+    //circles 6 to 1
+    circles.push(createCircle(250, 200, 20, "6"), 25);
+    for (let index = 0; index < 4; index++) {
+      accurCircles.push(
+        createAccurCircle(
+          240 - 10 * index,
+          180 - 20 * index,
+          20,
+          "6." + index.toString()
+        ),
+        26 + index
+      );
+    }
   }
 }
 
 // Define a function to create a circle with text
-function createCircle(x, y, diameter, label) {
+function createCircle(x, y, diameter, label, order) {
   fill(255);
   ellipse(x, y, diameter, diameter);
   fill(0);
   textSize(5);
   textAlign(CENTER, CENTER);
   text(label, x, y);
-  return { x, y, diameter, label, intersected: false };
+  return { x, y, diameter, label, order, intersected: false };
 }
-function createAccurCircle(x, y, diameter, label) {
+
+function createAccurCircle(x, y, diameter, label, order) {
   fill(255);
   ellipse(x, y, diameter, diameter);
   fill(0);
   textSize(5);
   textAlign(CENTER, CENTER);
   text(label, x, y);
-  return { x, y, diameter, label, intersected: false };
+  return { x, y, diameter, label, order, intersected: false };
 }
 
 function resetSelection() {
@@ -79,6 +113,10 @@ function resetSelection() {
 function draw() {
   stroke(255, 0, 0);
   touchMoved();
+
+  textSize(48);
+  textStyle(BOLD);
+  text(accuracy, 300, 40);
 }
 function touchMoved() {
   // your touch related logic here
@@ -126,24 +164,48 @@ function touchMoved() {
 }
 
 function checkAccuracy() {
-  circles.forEach(circle => {
+  let prevIntersected;
+
+  circles.forEach((circle) => {
     let circleCenter = createVector(circle.x, circle.y);
     let startDist = p5.Vector.dist(lineStart, circleCenter);
     let endDist = p5.Vector.dist(lineEnd, circleCenter);
-    if ((startDist <= circle.diameter / 2 || endDist <= circle.diameter / 2) && !circle.intersected) {
-      console.log(`The line intersects circle ${circle.label}`);
-      circle.intersected = true;
+    if (
+      (startDist <= circle.diameter / 2 || endDist <= circle.diameter / 2) &&
+      !circle.intersected &&
+      circle != prevIntersected
+    ) {
+      if (circle.order === currentOrder) {
+        console.log(`The line intersects circle ${circle.label}`);
+        circle.intersected = true;
+        prevIntersected = circle;
+        currentOrder++;
+      } else {
+        console.log(`Circle ${circle.label} was skipped`);
+        accuracy -= 10; // decrease accuracy by 10 for each skipped circle
+        currentOrder++;
+      }
     }
   });
-
-
-  accurCircles.forEach(circle => {
+  accurCircles.forEach((circle) => {
     let circleCenter = createVector(circle.x, circle.y);
     let startDist = p5.Vector.dist(lineStart, circleCenter);
     let endDist = p5.Vector.dist(lineEnd, circleCenter);
-    if ((startDist <= circle.diameter / 2 || endDist <= circle.diameter / 2) && !circle.intersected) {
-      console.log(`The line intersects circle ${circle.label}`);
-      circle.intersected = true;
+    if (
+      (startDist <= circle.diameter / 2 || endDist <= circle.diameter / 2) &&
+      !circle.intersected &&
+      circle != prevIntersected
+    ) {
+      if (circle.order === currentOrder) {
+        console.log(`The line intersects circle ${circle.label}`);
+        circle.intersected = true;
+        prevIntersected = circle;
+        currentOrder++;
+      } else {
+        console.log(`Circle ${circle.label} was skipped`);
+        accuracy -= 10; // decrease accuracy by 10 for each skipped circle
+        currentOrder++;
+      }
     }
   });
 }
