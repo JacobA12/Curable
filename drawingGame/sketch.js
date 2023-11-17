89fb2f161b4b6b2c8dc1a594e8f0997871598f8a
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2023-11-17 13:42:55 -0700

    im lost

--- drawingGame/sketch.js
+++ drawingGame/sketch.js
@@ -1,18 +1,26 @@
 let targetShape;
-let userPath = [];
 let score = 0;
 let currentLevel = 1;
+let isTracing = false;
+let threshold = 10;
+
+// Triangle vertices
+let v1;
+let v2;
+let v3;
 
 function setup() {
   createCanvas(600, 600);
-
+  v1 = createVector(300, 50);
+  v2 = createVector(100, 500);
+  v3 = createVector(500, 500);
   targetShape = createShape();
 }
 
 function draw() {
   background("white");
   touchMoved();
-  
+
   fill(204, 101, 192, 127);
   stroke(127, 63, 120);
   createShape(targetShape);
@@ -22,36 +30,69 @@ function draw() {
     ellipse(touches[0].x, touches[0].y, 40, 40, 45);
   }
 
+  if (!isTracing) {
+    // Add this block
+    score--;
+  }
 }
 
 function touchStarted() {
   userPath = [];
-
+  isTracing = true;
 
   return false;
 }
 
 function touchMoved() {
+  fill("red");
+  stroke(40);
   if (touches.length > 0) {
     let point = createVector(touches[0].x, touches[0].y);
-    userPath.push(point);
-    
+    drawnLine = line(touches[0].x, touches[0].y, pmouseX, pmouseY);
+
+    // Calculate the distance from the point to each line of the triangle
+    let d1 = distToSegment(point, v1, v2);
+    let d2 = distToSegment(point, v2, v3);
+    let d3 = distToSegment(point, v3, v1);
+
+    // If the point is close to any of the lines, the user is tracing the shape
+    if (d1 < threshold || d2 < threshold || d3 < threshold) {
+      isTracing = true;
+    } else {
+      isTracing = false;
+    }
   }
 }
 
 function touchEnded() {
   calculateScore();
+  isTracing = false; // Add this line
 }
 
 function calculateScore() {
-  // Implement your scoring algorithm here
-  // You can compare the userPath with the targetShape to determine accuracy
-  // Update the score variable accordingly
+  if (isTracing) {
+    // The user is tracing the shape correctly, so the score remains the same
+  } else {
+    // The user deviated from the trace, so they lose points
+    score--;
+  }
+  console.log(score);
+}
+function distToSegment(point, v, w) {
+  let l2 = distSq(v, w);
+  if (l2 === 0) return distSq(point, v);
+  let t = ((point.x - v.x) * (w.x - v.x) + (point.y - v.y) * (w.y - v.y)) / l2;
+  t = max(0, min(1, t));
+  return sqrt(
+    distSq(point, createVector(v.x + t * (w.x - v.x), v.y + t * (w.y - v.y)))
+  );
+}
+function distSq(v, w) {
+  return pow(v.x - w.x, 2) + pow(v.y - w.y, 2);
 }
 
 function createShape() {
-    if (currentLevel === 1) {
-      triangle(300, 200, 200, 400, 400, 400);
-    }
+  if (currentLevel === 1) {
+    triangle(v1.x, v1.y, v2.x, v2.y, v3.x, v3.y);
+  }
 }
-
